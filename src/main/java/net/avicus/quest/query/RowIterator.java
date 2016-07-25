febcc16d0892d2668093bcb7bfc62a5496377df7
thekeenant <keenan@keenant.com>
2016-07-24 23:28:00 -0500

    Fix RowIterator, isLast() cannot be used in stream

--- src/main/java/net/avicus/quest/query/RowIterator.java
+++ src/main/java/net/avicus/quest/query/RowIterator.java
@@ -6,11 +6,13 @@ import net.avicus.quest.database.DatabaseException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 @ToString
 public class RowIterator implements Iterator<Row> {
     private final ResultSet resultSet;
     private boolean started = false;
+    private Row current;
 
     public RowIterator(ResultSet resultSet) {
         this.resultSet = resultSet;
@@ -19,9 +21,17 @@ public class RowIterator implements Iterator<Row> {
     @Override
     public boolean hasNext() {
         try {
-            if (!this.started)
+            if (this.started) {
+                if (this.current != null)
+                    return true;
+                boolean next = this.resultSet.next();
+                if (next)
+                    this.current = new Row(this.resultSet);
+                return next;
+            }
+            else {
                 return this.resultSet.isBeforeFirst() || this.resultSet.getRow() != 0;
-            return !this.resultSet.isLast();
+            }
         } catch (SQLException e) {
             throw new DatabaseException(e);
         }
@@ -29,12 +39,11 @@ public class RowIterator implements Iterator<Row> {
 
     @Override
     public Row next() throws DatabaseException {
-        try {
-            this.started = true;
-            this.resultSet.next();
-            return new Row(this.resultSet);
-        } catch (SQLException e) {
-            throw new DatabaseException(e);
-        }
+        this.started = true;
+
+        if (!hasNext())
+            throw new NoSuchElementException();
+
+        return this.current;
     }
 }
